20075d96cbdc59d9930a112c0d32233dd35e89ca
sayali199918 <sayaliprashantbadgujar@gmail.com>
2020-07-20 07:31:25 -0700

    [sayali] Added:functionality for calculating wages for month

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,18 +1,23 @@
 public class EmployeeWage {
 	public static void main(String[] args) {
+		//constants
 		int IS_FULL_TIME = 1;
 		int IS_PART_TIME = 2;
 		int WAGE_PER_HOUR = 20;
 
+		//variables
 		int dailyWage=0;
 		int empHrs=0;
 		int totalWage=0;
+		int totalWorkingHrs = 0;
+		int totalWorkingDays = 0;
 
+		//computation
 		System.out.println("Welcome to Employee Wage Program");
-		for(int i=0; i<20; i++)
+		while(totalWorkingHrs <= 100 && totalWorkingDays <= 20)
 		{
-			int empCheck = (int)Math.floor(Math.random() * 10 ) %3;
-			System.out.println(empCheck);
+			totalWorkingDays++;
+			int empCheck = (int)Math.floor(Math.random() * 10)%3;
 			switch (empCheck)
 			{
 				case 0:
@@ -25,10 +30,11 @@ public class EmployeeWage {
 					empHrs=8;
 					break;
 			}
-			dailyWage=WAGE_PER_HOUR*empHrs;
-			totalWage=totalWage+dailyWage;
+			totalWorkingHrs+=empHrs;
+			System.out.println("Days: " +totalWorkingDays+ "Employees hrs: "+empHrs);
 		}
-		System.out.println("Monthly wage of employee:"+ totalWage);
+		totalWage=totalWorkingHrs*WAGE_PER_HOUR;
+		System.out.println("Total wage of employee:"+totalWage);
 	}
 
 }
