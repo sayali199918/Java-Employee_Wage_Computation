dc86f335b39c3d5f3e81d3606aa828f33f1bcd1a
sayali199918 <sayaliprashantbadgujar@gmail.com>
2020-07-20 06:57:28 -0700

    [sayali] Added:Monthly Employee Wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,26 +4,31 @@ public class EmployeeWage {
 		int IS_PART_TIME = 2;
 		int WAGE_PER_HOUR = 20;
 
-		int empCheck = (int)Math.floor(Math.random() * 10 ) %3;
 		int dailyWage=0;
 		int empHrs=0;
+		int totalWage=0;
 
 		System.out.println("Welcome to Employee Wage Program");
-		switch (empCheck)
+		for(int i=0; i<20; i++)
 		{
-		case 0:
-			empHrs=0;
-			break;
-		case 1:
-			empHrs=4;
-			break;
-		case 2:
-			empHrs=8;
-			break;
+			int empCheck = (int)Math.floor(Math.random() * 10 ) %3;
+			System.out.println(empCheck);
+			switch (empCheck)
+			{
+				case 0:
+					empHrs=0;
+					break;
+				case 1:
+					empHrs=4;
+					break;
+				case 2:
+					empHrs=8;
+					break;
+			}
+			dailyWage=WAGE_PER_HOUR*empHrs;
+			totalWage=totalWage+dailyWage;
 		}
-		dailyWage=WAGE_PER_HOUR*empHrs;
-		System.out.println("Daily wage of employee:"+ dailyWage);
-		
+		System.out.println("Monthly wage of employee:"+ totalWage);
 	}
 
 }
