ec716005ace0b0e39f5b99e32f1ad094ff1fb0e8
sayali199918 <sayaliprashantbadgujar@gmail.com>
2020-07-20 06:03:54 -0700

    [sayali] Added:Daily Employee Wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,12 +1,21 @@
 public class EmployeeWage {
 	public static void main(String[] args) {
 		int IS_FULL_TIME = 1;
+		int WAGE_PER_HOUR=20;
+		int FULL_DAY_HOUR=8;
 		double empCheck = Math.floor(Math.random() * 10 ) %2;
+		int dailyWage=0;
+
 		System.out.println("Welcome to Employee Wage Program");
-		if (empCheck == IS_FULL_TIME)
+		if (empCheck == IS_FULL_TIME) {
 			System.out.println("Employee is Present.");
-		else
+			dailyWage=WAGE_PER_HOUR*FULL_DAY_HOUR;
+		}
+		else 
+		{
 			System.out.println("Employee is Absent.");
+		}
+		System.out.println("daily wage of employee is:" + dailyWage);
 	}
 
 }
