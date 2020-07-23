07224bc4a5ec19d909fb03a18dd29adb780ef876
sayali199918 <sayaliprashantbadgujar@gmail.com>
2020-07-23 09:46:55 -0700

    Added:functionality of class method

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,21 +1,20 @@
 public class EmployeeWage {
-	public static void main(String[] args) {
-		//constants
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;
-		int WAGE_PER_HOUR = 20;
 
-		//variables
+		public static final int IS_PART_TIME = 1;
+		public static final int IS_FULL_TIME = 2;
+		public static final int WAGE_PER_HOUR = 20;
+		
+		public static int computeEmployeeWage() {
 		int dailyWage=0;
 		int empHrs=0;
-		int totalWage=0;
 		int totalWorkingHrs = 0;
 		int totalWorkingDays = 0;
-
+		
 		//computation
 		System.out.println("Welcome to Employee Wage Program");
 		while(totalWorkingHrs <= 100 && totalWorkingDays <= 20)
 		{
+			int totalWage=0;
 			totalWorkingDays++;
 			int empCheck = (int)Math.floor(Math.random() * 10)%3;
 			switch (empCheck)
@@ -30,11 +29,17 @@ public class EmployeeWage {
 					empHrs=8;
 					break;
 			}
+			
 			totalWorkingHrs+=empHrs;
-			System.out.println("Days: " +totalWorkingDays+ "Employees hrs: "+empHrs);
+			System.out.println("Days:"+totalWorkingDays+   " Employees hrs: "+empHrs);
 		}
-		totalWage=totalWorkingHrs*WAGE_PER_HOUR;
+		int totalWage=totalWorkingHrs*WAGE_PER_HOUR;
 		System.out.println("Total wage of employee:"+totalWage);
+		return totalWage;
+		
 	}
-
+		public static void main(String[] args) {
+			computeEmployeeWage();
+		}
 }
+
