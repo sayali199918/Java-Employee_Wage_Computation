2a3c8d575818355c4542b0ba1dc8f22dfb202a4c
sayali199918 <sayaliprashantbadgujar@gmail.com>
2020-07-24 08:44:49 -0700

    Compute Employee Wage for multiple companies

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,45 +1,61 @@
-public class EmployeeWage {
+import java.util.Scanner;
+public class EmployeeWage{
 
-		public static final int IS_PART_TIME = 1;
-		public static final int IS_FULL_TIME = 2;
-		public static final int WAGE_PER_HOUR = 20;
-		
-		public static int computeEmployeeWage() {
-		int dailyWage=0;
-		int empHrs=0;
-		int totalWorkingHrs = 0;
-		int totalWorkingDays = 0;
-		
-		//computation
-		System.out.println("Welcome to Employee Wage Program");
-		while(totalWorkingHrs <= 100 && totalWorkingDays <= 20)
-		{
-			int totalWage=0;
-			totalWorkingDays++;
-			int empCheck = (int)Math.floor(Math.random() * 10)%3;
-			switch (empCheck)
-			{
-				case 0:
-					empHrs=0;
-					break;
-				case 1:
-					empHrs=4;
-					break;
-				case 2:
-					empHrs=8;
-					break;
-			}
-			
-			totalWorkingHrs+=empHrs;
-			System.out.println("Days:"+totalWorkingDays+   " Employees hrs: "+empHrs);
-		}
-		int totalWage=totalWorkingHrs*WAGE_PER_HOUR;
-		System.out.println("Total wage of employee:"+totalWage);
-		return totalWage;
-		
-	}
-		public static void main(String[] args) {
-			computeEmployeeWage();
-		}
+        private static void employeeMonthlyWage(String companyName, int WAGE_PER_HOUR,int workingHr,int MAX_HR_IN_MONTH,int NUM_OF_WORKING_DAYS) {
+                //variable
+                int  empHrs=0;
+                int  empWage=0;
+                int  totalWorkingHrs=0;
+                int  totalWorkingDays=0;
+                int  totalWage=0;
+
+                while(totalWorkingHrs <= MAX_HR_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS)
+                        {
+
+                                totalWorkingDays++;
+                                int empCheck = (int)Math.floor(Math.random() * 10)%3;
+                                switch (empCheck)
+                                {
+                                        case 0:
+                                                empHrs=0;
+                                                break;
+                                        case 1:
+                                                empHrs=4;
+                                                break;
+                                        case 2:
+                                                empHrs=8;
+                                                break;
+                                }
+
+                                totalWorkingHrs+=empHrs;
+                                empWage=empHrs*WAGE_PER_HOUR;
+                                totalWage+=empWage;
+                        }
+
+                        System.out.println("Monthly wage of employee of "+companyName+" is:"+totalWage);
+
+                }
+
+        public static void main(String args[]){
+                Scanner sc=new Scanner(System.in);
+                System.out.println("Welcome to Employee Wage Computation");
+                System.out.println("Enter the number of Companies:");
+                int number=sc.nextInt();
+                for (int i=1;i<=number;i++)
+                {
+                System.out.println("Enter the Company Name");
+		 String companyName=sc.next();
+                System.out.println("Enter Wage per Hour");
+                int WAGE_PER_HOUR=sc.nextInt();
+                System.out.println("Enter Full day Hour");
+                int workingHr=sc.nextInt();
+                System.out.println("Enter Total Hours");
+                int MAX_HR_IN_MONTH=sc.nextInt();
+                System.out.println("Enter Day Per Month");
+                int NUM_OF_WORKING_DAYS=sc.nextInt();
+                employeeMonthlyWage(companyName,WAGE_PER_HOUR,workingHr,MAX_HR_IN_MONTH,NUM_OF_WORKING_DAYS);
+                        }
+                }
+
 }
 
